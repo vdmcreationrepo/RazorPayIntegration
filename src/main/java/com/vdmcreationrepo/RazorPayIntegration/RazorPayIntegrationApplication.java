b5839732f024054eb813dbb250a856ffae309107
VIDESH MAKWANA <videsh.m@brilworks.com>
2024-05-08 16:07:56 +0530

    first commit

---
+++ src/main/java/com/vdmcreationrepo/RazorPayIntegration/RazorPayIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.vdmcreationrepo.RazorPayIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazorPayIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazorPayIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vdmcreationrepo/RazorPayIntegration/RazorPayIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdmcreationrepo.RazorPayIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazorPayIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
